5d9507ef8ac65901ff881f3444b567763c937c33
EstebanCaponio <estebancaponio@gmail.com>
2025-04-11 13:47:29 +0200

    feat: milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+
+const endpoint = 'https://www.freetestapi.com/api/v1/actresses';
 
 function App() {
+
+  const [actressesList, setActressesList] = useState([]);
+
+  function fetchActresses() {
+    axios.get(endpoint)
+      .then(res => (
+        setActressesList(res.data),
+        console.log(res.data)))
+  }
+
+  useEffect(fetchActresses, []);
 
   return (
     <>
       <div>
-
+        <ul>
+          {actressesList.map(actress => (
+            <li key={actress.id}> {actress.name}</li>
+          ))}
+        </ul>
       </div>
     </>
   )
